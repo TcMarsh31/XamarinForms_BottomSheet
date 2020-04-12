2b944f2bda4f076d3e404fcc1ffdd484122e5236
panchanathantharsan <tharsan.3112@gmail.com>
2020-04-12 16:04:52 +0530

    * Monkey.cs: * Slice 2.png: * logo.png: * SplashScreen.xml:
    * BottomSheet.Android.csproj: Bottom sheet implemented

---
+++ BottomSheet/Monkey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BottomSheet
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
