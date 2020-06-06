2bebbd2ad30ac7e1640153f5fbdef5c7c6690e8b
panchanathantharsan <tharsan.3112@gmail.com>
2020-06-06 18:21:30 +0530

    * CustomFrameRenderer.cs: android CustomRenderer added
    * Resource.designer.cs:

---
+++ BottomSheet.Android/CustomFrameRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System;
+using Android.Content;
+using BottomSheet;
+using BottomSheet.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomFrame), typeof(CustomFrameRenderer))]
+namespace BottomSheet.Droid
+{
+    public class CustomFrameRenderer : Xamarin.Forms.Platform.Android.AppCompat.FrameRenderer
+    {
+        public CustomFrameRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            try
+            {
+                var elem = (CustomFrame)this.Element;
+                if (elem != null)
+                {
+                    //shadow
+                    CardElevation = 30f;
+                    TranslationZ = 0;
+                    SetZ(30f);
+                    //
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
